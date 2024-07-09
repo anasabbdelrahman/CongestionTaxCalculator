6312848eac80c77aaf81ea0b9b5091681678ac1d
Anas Abdelrahman <73756229+anasabbdelrahman@users.noreply.github.com>
2024-07-09 20:14:33 +0200

    Update CongestionTaxController.java to handle wrong vehicle type

--- src/main/java/congestion/calculator/CongestionTaxController.java
+++ src/main/java/congestion/calculator/CongestionTaxController.java
@@ -1,6 +1,10 @@
 package congestion.calculator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
@@ -13,12 +17,22 @@ public class CongestionTaxController {
 
     @Autowired
     private CongestionTaxCalculator congestionTaxCalculator;
+    private static final Logger logger = LoggerFactory.getLogger(CongestionTaxController.class);
 
     @GetMapping("/calculate")
-    public int calculateTax(@RequestParam String vehicleType, @RequestParam String[] dates) {
-        Vehicle vehicle = createVehicle(vehicleType);
-        Date[] dateArray = parseDates(dates);
-        return congestionTaxCalculator.getTax(vehicle, dateArray);
+    public ResponseEntity<?> calculateTax(@RequestParam String vehicleType, @RequestParam String[] dates) {
+        try {
+            Vehicle vehicle = createVehicle(vehicleType);
+            Date[] dateArray = parseDates(dates);
+            int tax = congestionTaxCalculator.getTax(vehicle, dateArray);
+            return new ResponseEntity<>(tax, HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            logger.error("Unknown vehicle type: " + vehicleType, e);
+            return new ResponseEntity<>("Unknown vehicle type: " + vehicleType, HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            logger.error("Error occurred while calculating tax", e);
+            return new ResponseEntity<>("An error occurred while calculating the tax", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     private Vehicle createVehicle(String vehicleType) {
